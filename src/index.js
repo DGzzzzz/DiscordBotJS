7ffdd33efffa49afc6c01c66af9efd22720ea6ab
DGzzzzz <douglaswinter@outlook.com>
2024-09-29 10:49:48 -0300

    :recycle: refactor: code to use express for server setup and listen on specified port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
+const express = require('express');
+const app = express();
 const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+{
+    console.log(`🚀 Servidor rodando na porta ${port}`);
+})
 
 const client = new Client({
     intents: [
